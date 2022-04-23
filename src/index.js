0bf8ac039f18a67efec6d7fd56fc82f085802f01
Joe Thompson <jthompso@mtu.edu>
2022-04-23 16:24:44 -0700

    Added HashRouter to fix blank page on github pages.

--- src/index.js
+++ src/index.js
@@ -9,11 +9,14 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <HashRouter base="/">
+        <App />
+      </HashRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
