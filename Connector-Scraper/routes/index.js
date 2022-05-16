a947487f78a4dc77e963e8f5b0f5a64588569e50
Joe Thompson <jthompson7@blueorigin.com>
2022-05-16 10:56:49 -0700

    Get number of results from digi-key.

--- Connector-Scraper/routes/index.js
+++ Connector-Scraper/routes/index.js
@@ -3,27 +3,37 @@ var router = express.Router();
 const puppeteer = require('puppeteer');
 
 let browser = null;
+let context = null;
+const headless = true;  // set false to view browser during search
 
 /* GET home page. */
 router.get('/:partNum', async function(req, res, next) {
-  let partNum = req.params.partNum;
+  const partNum = req.params.partNum;
 
-  if (!browser) browser = await puppeteer.launch({headless: false});
+  if (!browser) {
+    browser = await puppeteer.launch({headless: headless});
+    context = await browser.createIncognitoBrowserContext();
+  }
 
-  const page = await browser.newPage();
+  const page = await context.newPage();
+
+  // digi-key part search
   await page.goto('https://www.digikey.com/en/products/filter/circular-connectors/436');
-  const searchBox = 'text[data-testid="search-input"]';
-  await page.waitForSelector('.jss278', {visible: true});
-  await page.$('.jss278', (el, value) => el.value = value, partNum);
-  //await page.$eval('input[name=search]', (el, value) => el.value = value, myLocalValue);
-  //await page.waitForSelector(searchBox);
-  //await page.$(searchBox, (el, value) => el.value = value, partNum);
-  //const searchButton = 'button[data-testid="search-input-submit"]';
-  //page.click(searchButton);
+  const searchInputSel = 'input[data-testid="search-input"]';
+  await page.waitForSelector(searchInputSel, {visible: true});
+  await page.type(searchInputSel, partNum);
+  const searchButtonSel = 'button[data-testid="search-input-submit"]';
+  await page.click(searchButtonSel);
+  await page.waitForNavigation();
 
-  //await page.close();
+  // scrape search results
+  const numResultsSel = 'span[data-testid="product-count"]';
+  const element = await page.waitForSelector(numResultsSel, {visible: true});
+  const value = await element.evaluate(el => el.textContent);
 
-  res.send(partNum);
+  await page.close();
+
+  res.send(value);
 });
 
 module.exports = router;
