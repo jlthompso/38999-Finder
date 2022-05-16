612c712b7afa40f640ed0410795304486e2c8e75
Joe Thompson <jthompso@mtu.edu>
2022-05-16 09:01:25 -0700

    Wait for browser in get request.

--- Connector-Scraper/routes/index.js
+++ Connector-Scraper/routes/index.js
@@ -2,11 +2,26 @@ var express = require('express');
 var router = express.Router();
 const puppeteer = require('puppeteer');
 
-const browser = puppeteer.launch();
+let browser = null;
 
 /* GET home page. */
-router.get('/:partNum', function(req, res, next) {
+router.get('/:partNum', async function(req, res, next) {
   let partNum = req.params.partNum;
+
+  if (!browser) browser = await puppeteer.launch({headless: false});
+
+  const page = await browser.newPage();
+  await page.goto('https://www.digikey.com/en/products/filter/circular-connectors/436');
+  const searchBox = 'text[data-testid="search-input"]';
+  await page.waitForSelector('.jss278', {visible: true});
+  await page.$('.jss278', (el, value) => el.value = value, partNum);
+  //await page.$eval('input[name=search]', (el, value) => el.value = value, myLocalValue);
+  //await page.waitForSelector(searchBox);
+  //await page.$(searchBox, (el, value) => el.value = value, partNum);
+  //const searchButton = 'button[data-testid="search-input-submit"]';
+  //page.click(searchButton);
+
+  //await page.close();
 
   res.send(partNum);
 });
