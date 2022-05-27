a6dec1ad4c9bb48d12813eef8e7a80b96fc93191
Joe Thompson <jthompson7@blueorigin.com>
2022-05-27 11:29:58 -0700

    Added img loading indicator.

--- src/components/InsertImg.js
+++ src/components/InsertImg.js
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { selectShellSize, selectInsertArrangement } from './connectorSlice';
+import Box from '@mui/material/Box';
+import Fade from '@mui/material/Fade';
+import CircularProgress from '@mui/material/CircularProgress';
 
 const imgSvc = 'https://image-38999.herokuapp.com/38999/';
 
 export default function InsertImg() {
   const [imgUrl, setImgUrl] = useState();
+  const [loading, setLoading] = useState(false);
 
   const shellSize = useSelector(selectShellSize);
   const insertArrangement = useSelector(selectInsertArrangement);
@@ -15,6 +19,8 @@ export default function InsertImg() {
   }, [insertArrangement]);
 
   const fetchConnImg = async () => {
+    setLoading(true);
+
     let milShellSize;
     switch (shellSize) {
       case 9:
@@ -50,14 +56,32 @@ export default function InsertImg() {
     };
 
     const url = `${imgSvc}${milShellSize}${String(insertArrangement)}`;
+    // await new Promise(resolve => setTimeout(resolve, 5000));  // uncomment to force loading indicator
     const response = await fetch(url);
     const jsonData = await response.json();
     const imgUrl = jsonData[`${milShellSize}${String(insertArrangement)}`];
-
     setImgUrl(imgUrl);
+
+    setLoading(false);
   };
 
   return (
-    <img src={imgUrl} alt="Front Face of Pin Insert" width="60%" />
+    <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <Box sx={{ height: 40 }}>
+        {loading ? (
+          <Fade
+            in={loading}
+            style={{
+              transitionDelay: loading ? '800ms' : '0ms',
+            }}
+            unmountOnExit
+          >
+            <CircularProgress />
+          </Fade>
+        ) : (
+          <img src={imgUrl} alt="Front Face of Pin Insert" width="60%" />
+        )}
+      </Box>
+    </Box>
   );
 }
