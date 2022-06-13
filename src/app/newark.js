2fc9d1363c20700a528b03c19981e666868b0c3d
Joe Thompson <jthompson7@blueorigin.com>
2022-06-13 09:56:35 -0700

    Get jsonp results from Newark.

--- src/app/newark.js
+++ src/app/newark.js
@@ -1,3 +1,5 @@
+import fetchJsonp from 'fetch-jsonp';
+
 export async function search(partNum) {
   const results = [];
 
@@ -7,22 +9,32 @@ export async function search(partNum) {
               `&storeInfo.id=www.newark.com` +
               `&resultsSettings.refinements.filters=inStock` +
               `&resultsSettings.responseGroup=medium` +
+              `&callInfo.callback=getSearchResults` +
               `&callInfo.responseDataFormat=json` +
-              `&callinfo.apiKey=${process.env.REACT_APP_NEWARK_API_KEY}`
+              `&callinfo.apiKey=${process.env.REACT_APP_NEWARK_API_KEY}`;
 
-  const response = await fetch(url);
 
-  const json = await response.json();
-  json.manufacturerPartNumberSearchReturn.products.forEach(product => {
-    const vendor = "Newark";
-    const qty = product.stock.level;
-    const price = `$${product.prices[0].cost}`;
-    const id = product.id;
-    const mfgPartNum = product.translatedManufacturerPartNumber;
-    const link = `https://www.newark.com/${product.sku}`;
-    const mfgr = product.vendorName;
-
-    if (mfgPartNum === partNum && qty > 0) results.push({vendor, qty, price, id, partNum: mfgPartNum, link, mfgr});
+  fetchJsonp(url, {jsonpCallbackFunction: 'getSearchResults'})
+  .then(function(response) {
+    return response.json();
+  }).then(function(json) {
+    if (json.hasOwnProperty('manufacturerPartNumberSearchReturn')
+      && json.hasOwnProperty('prices')
+      && json.manufacturerPartNumberSearchReturn.numberOfResults) {
+      json.manufacturerPartNumberSearchReturn.products.forEach(product => {
+        const vendor = "Newark";
+        const qty = product.stock.level;
+        const price = `$${product.prices[0].cost}`;
+        const id = product.id;
+        const mfgPartNum = product.translatedManufacturerPartNumber;
+        const link = `https://www.newark.com/${product.sku}`;
+        const mfgr = product.vendorName;
+  
+        if (mfgPartNum === partNum && qty > 0) results.push({vendor, qty, price, id, partNum: mfgPartNum, link, mfgr});
+      });
+    }
+  }).catch(function(ex) {
+    console.log('parsing failed', ex);
   });
 
   return results;
